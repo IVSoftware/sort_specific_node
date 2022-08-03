f861c780a982e80c49883b4b53dacd4f439322ca
Tom Gregor <tgregor@ivsoftware.com>
2022-08-03 08:01:23 -0600

    Add project files.

---
+++ sort_specific_node/MainForm.Designer.cs
@@ -0,0 +1,260 @@
+﻿
+namespace sort_specific_node
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node0");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("A");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Z");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("N");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Y");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("H");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("z", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("b");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("t");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode7,
+            treeNode8,
+            treeNode9});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Node4");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Node5");
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.checkBoxSorted = new System.Windows.Forms.CheckBox();
+            this.buttonSortNode1 = new System.Windows.Forms.Button();
+            this.buttonSortZ = new System.Windows.Forms.Button();
+            this.buttonSortZDesc = new System.Windows.Forms.Button();
+            this.textBoxList = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelNode1 = new System.Windows.Forms.Label();
+            this.labelAsc = new System.Windows.Forms.Label();
+            this.labelDesc = new System.Windows.Forms.Label();
+            this.labelZ = new System.Windows.Forms.Label();
+            this.buttonSortNode1Desc = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView
+            // 
+            this.treeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            treeNode1.Name = "Node0";
+            treeNode1.Text = "Node0";
+            treeNode2.Name = "A";
+            treeNode2.Text = "A";
+            treeNode3.Name = "Z";
+            treeNode3.Text = "Z";
+            treeNode4.Name = "N";
+            treeNode4.Text = "N";
+            treeNode5.Name = "Y";
+            treeNode5.Text = "Y";
+            treeNode6.Name = "H";
+            treeNode6.Text = "H";
+            treeNode7.Name = "z";
+            treeNode7.Text = "z";
+            treeNode8.Name = "b";
+            treeNode8.Text = "b";
+            treeNode9.Name = "t";
+            treeNode9.Text = "t";
+            treeNode10.Name = "b";
+            treeNode10.Text = "Node1";
+            treeNode11.Name = "Node4";
+            treeNode11.Text = "Node4";
+            treeNode12.Name = "Node5";
+            treeNode12.Text = "Node5";
+            this.treeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode10,
+            treeNode11,
+            treeNode12});
+            this.treeView.Size = new System.Drawing.Size(290, 452);
+            this.treeView.TabIndex = 0;
+            // 
+            // checkBoxSorted
+            // 
+            this.checkBoxSorted.AutoSize = true;
+            this.checkBoxSorted.Location = new System.Drawing.Point(318, 11);
+            this.checkBoxSorted.Name = "checkBoxSorted";
+            this.checkBoxSorted.Size = new System.Drawing.Size(91, 29);
+            this.checkBoxSorted.TabIndex = 1;
+            this.checkBoxSorted.Text = "Sorted";
+            this.checkBoxSorted.UseVisualStyleBackColor = true;
+            this.checkBoxSorted.CheckedChanged += new System.EventHandler(this.checkBoxSorted_CheckedChanged);
+            // 
+            // buttonSortNode1
+            // 
+            this.buttonSortNode1.Location = new System.Drawing.Point(99, 69);
+            this.buttonSortNode1.Name = "buttonSortNode1";
+            this.buttonSortNode1.Size = new System.Drawing.Size(82, 32);
+            this.buttonSortNode1.TabIndex = 2;
+            this.buttonSortNode1.UseVisualStyleBackColor = true;
+            this.buttonSortNode1.Click += new System.EventHandler(this.buttonSortNode1_Click);
+            // 
+            // buttonSortZ
+            // 
+            this.buttonSortZ.Location = new System.Drawing.Point(99, 30);
+            this.buttonSortZ.Name = "buttonSortZ";
+            this.buttonSortZ.Size = new System.Drawing.Size(82, 32);
+            this.buttonSortZ.TabIndex = 2;
+            this.buttonSortZ.UseVisualStyleBackColor = true;
+            this.buttonSortZ.Click += new System.EventHandler(this.buttonSortZAsc_Click);
+            // 
+            // buttonSortZDesc
+            // 
+            this.buttonSortZDesc.Location = new System.Drawing.Point(188, 30);
+            this.buttonSortZDesc.Name = "buttonSortZDesc";
+            this.buttonSortZDesc.Size = new System.Drawing.Size(83, 32);
+            this.buttonSortZDesc.TabIndex = 2;
+            this.buttonSortZDesc.UseVisualStyleBackColor = true;
+            this.buttonSortZDesc.Click += new System.EventHandler(this.buttonSortZDesc_Click);
+            // 
+            // textBoxList
+            // 
+            this.textBoxList.Font = new System.Drawing.Font("Segoe UI", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxList.Location = new System.Drawing.Point(318, 166);
+            this.textBoxList.Multiline = true;
+            this.textBoxList.Name = "textBoxList";
+            this.textBoxList.Size = new System.Drawing.Size(275, 266);
+            this.textBoxList.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 94F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.labelNode1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelAsc, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelDesc, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSortZDesc, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSortZ, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSortNode1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelZ, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSortNode1Desc, 2, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(318, 55);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(275, 105);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // labelNode1
+            // 
+            this.labelNode1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelNode1.Location = new System.Drawing.Point(4, 66);
+            this.labelNode1.Name = "labelNode1";
+            this.labelNode1.Size = new System.Drawing.Size(88, 38);
+            this.labelNode1.TabIndex = 5;
+            this.labelNode1.Text = "\"Node1\"";
+            this.labelNode1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelAsc
+            // 
+            this.labelAsc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelAsc.Location = new System.Drawing.Point(99, 1);
+            this.labelAsc.Name = "labelAsc";
+            this.labelAsc.Size = new System.Drawing.Size(82, 25);
+            this.labelAsc.TabIndex = 3;
+            this.labelAsc.Text = "Asc";
+            this.labelAsc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDesc
+            // 
+            this.labelDesc.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelDesc.Location = new System.Drawing.Point(188, 1);
+            this.labelDesc.Name = "labelDesc";
+            this.labelDesc.Size = new System.Drawing.Size(83, 25);
+            this.labelDesc.TabIndex = 1;
+            this.labelDesc.Text = "Desc";
+            this.labelDesc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelZ
+            // 
+            this.labelZ.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelZ.Location = new System.Drawing.Point(4, 27);
+            this.labelZ.Name = "labelZ";
+            this.labelZ.Size = new System.Drawing.Size(88, 38);
+            this.labelZ.TabIndex = 4;
+            this.labelZ.Text = "\"z\"";
+            this.labelZ.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonSortNode1Desc
+            // 
+            this.buttonSortNode1Desc.Location = new System.Drawing.Point(188, 69);
+            this.buttonSortNode1Desc.Name = "buttonSortNode1Desc";
+            this.buttonSortNode1Desc.Size = new System.Drawing.Size(82, 32);
+            this.buttonSortNode1Desc.TabIndex = 2;
+            this.buttonSortNode1Desc.UseVisualStyleBackColor = true;
+            this.buttonSortNode1Desc.Click += new System.EventHandler(this.buttonSortNode1Desc_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 444);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.textBoxList);
+            this.Controls.Add(this.checkBoxSorted);
+            this.Controls.Add(this.treeView);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.CheckBox checkBoxSorted;
+        private System.Windows.Forms.Button buttonSortNode1;
+        private System.Windows.Forms.Button buttonSortZ;
+        private System.Windows.Forms.Button buttonSortZDesc;
+        private System.Windows.Forms.TextBox textBoxList;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelNode1;
+        private System.Windows.Forms.Label labelAsc;
+        private System.Windows.Forms.Label labelDesc;
+        private System.Windows.Forms.Label labelZ;
+        private System.Windows.Forms.Button buttonSortNode1Desc;
+    }
+}
+
---
+++ sort_specific_node/MainForm.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace sort_specific_node
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            treeView.Invalidated += (sender, e) => checkBoxSorted.Checked = treeView.Sorted;
+            treeView.ExpandAll();
+            treeView.Iterate(Index, new IndexArgs());
+        }
+        bool LogPath(TreeNode node, object unused)
+        {
+            var labels = new List<string>() { node.Text };
+
+            var traverse = node.Parent;
+            while (traverse != null)
+            {
+                labels.Insert(0, traverse.Text);
+                traverse = traverse.Parent;
+            }
+            var path = string.Join(@"\", labels);
+
+            textBoxList.AppendText($"{path}{Environment.NewLine}");
+            return false;
+        }
+        bool SortIfNodeMatch(TreeNode node, SortIfNodeMatchArgs args)
+        {
+            if((args.Predicate == null) || (args.Predicate(node)))
+            {
+                var list = node.Nodes.Cast<TreeNode>().ToList();
+                list.Sort((a, b) => args.Sorter(a, b));
+                node.Nodes.Clear();
+                foreach (var sorted in list)
+                {
+                    node.Nodes.Add(sorted);
+                }
+                return args.Predicate != null;
+            }
+            return false;
+        }
+        // Overloaded so that the arg can be 'object'
+        bool SortIfNodeMatch(TreeNode node, object args) =>
+            SortIfNodeMatch(node, (SortIfNodeMatchArgs)args);
+        class SortIfNodeMatchArgs
+        {
+            public Predicate<TreeNode> Predicate { get; set; }
+            public Func<TreeNode, TreeNode, int> Sorter { get; set; }
+        }
+        bool Index(TreeNode node, IndexArgs args)
+        {
+            node.Tag = args.Index++;
+            return false;
+        }
+        // Overloaded so that the arg can be 'object'
+        bool Index(TreeNode node, object args) =>
+            Index(node, (IndexArgs)args);
+
+        class IndexArgs
+        {
+            public int Index { get; set; }
+        }
+
+        private void checkBoxSorted_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBoxSorted.Checked)
+            {
+                treeView.Sort();
+            }
+            else
+            {
+                treeView.Sorted = false;
+                revert();
+            }
+            treeView.Iterate(LogPath, null);
+        }
+
+        private void revert()
+        {
+            var args =
+               new SortIfNodeMatchArgs
+               {
+                   Predicate = null,
+
+                   // This will sort in the original order.
+                   Sorter = (a, b) => ((int)a.Tag).CompareTo(((int)b.Tag)),
+               };
+            treeView.Iterate(SortIfNodeMatch, args);
+        }
+
+        private void buttonSortZAsc_Click(object sender, EventArgs e)
+        {
+            var args =
+               new SortIfNodeMatchArgs
+               {
+                   Predicate = (node) => node.Text == "z",
+
+                   // This will sort in the order of H N Y Z
+                   Sorter = (a, b) => a.Text.CompareTo(b.Text),
+               };
+            treeView.Iterate(SortIfNodeMatch, args);
+            treeView.Iterate(LogPath, null);
+        }
+
+        private void buttonSortZDesc_Click(object sender, EventArgs e)
+        {
+            var args =
+               new SortIfNodeMatchArgs
+               {
+                   Predicate = (node) => node.Text == "z",
+
+                   Sorter = (a, b) => b.Text.CompareTo(a.Text),
+               };
+            treeView.Iterate(SortIfNodeMatch, args);
+            treeView.Iterate(LogPath, null);
+        }
+
+        private void buttonSortNode1_Click(object sender, EventArgs e)
+        {
+            var args =
+               new SortIfNodeMatchArgs
+               {
+                    Predicate = (node) => node.Text == "Node1",
+
+                    // This will sort in the order of H N Y Z
+                    Sorter = (a, b) => a.Text.CompareTo(b.Text),
+               };
+            treeView.Iterate(SortIfNodeMatch, args);
+            treeView.Iterate(LogPath, null);
+        }
+
+        private void buttonSortNode1Desc_Click(object sender, EventArgs e)
+        {
+            var args =
+               new SortIfNodeMatchArgs
+               {
+                   Predicate = (node) => node.Text == "Node1",
+
+                   // This will sort in the order of H N Y Z
+                   Sorter = (a, b) => b.Text.CompareTo(a.Text),
+               };
+            treeView.Iterate(SortIfNodeMatch, args);
+            treeView.Iterate(LogPath, null);
+        }
+    }
+
+    delegate bool Fx(TreeNode node, object args);
+    static class Extensions
+    {
+        public static bool Iterate(
+            this object unk,    // A TreeView or TreeNode
+            Fx fx,              // A function that returns true to stop iteration
+            object args
+         )
+        {
+            TreeNodeCollection nodes;
+            if (unk is TreeNode treeNode)
+            {
+                if (fx(treeNode, args)) return true;
+                nodes = treeNode.Nodes;
+            }
+            else if (unk is TreeView treeView)
+            {
+                nodes = treeView.Nodes;
+            }
+            else throw new NotImplementedException($"Invalid type '{unk.GetType().Name}'");
+
+            foreach (TreeNode node in nodes)
+            {
+                if(node.Iterate(fx, args)) return true;
+            }
+            return false;
+        }
+    }
+}
